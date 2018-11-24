025cb266b2dfede199d43101c802cf45f7fb007d
drop-dan <daniel@earnwithdrop.com>
2018-11-24 13:35:42 -0500

    Adds bf last chance no desktop

---
+++ emails/black-friday-last-chance-no-desktop/us.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Main, Header, HeroImage, Footer, ItemRow, Button, Image } from '../../src/components'
+
+const BlackFridayEmail = () => (
+  <Main preheaderText="Sunday Funday, elevated. It's your last day to do some serious shopping damage. Score the best deals on your favorite brands - only with Drop. Shop our picks below via desktop, tablet, or mobile - all from the comfort of your couch. ">
+    <Header />
+
+    <HeroImage href="https://b.ewd.io/BFLastChance" image="black-friday-last-chance/header.gif" />
+
+    <div className="p2 mx-auto" style={{ maxWidth: 480 }}>
+      <h2 className="mt4 mb0">Hurry, promotions end tonight ⏳</h2>
+
+      <p className="my2">
+        Sunday Funday, elevated. It's your last day to do some serious shopping damage. Score the
+        best deals on your favorite brands - only with Drop. Shop our picks below via desktop,
+        tablet, or mobile - all from the comfort of your couch.
+      </p>
+
+      <Button
+        href="https://b.ewd.io/BFLastChance"
+        className="mx-auto mt3"
+        style={{ maxWidth: 300 }}
+      >
+        Shop Black Friday
+      </Button>
+
+      <div className="divider" />
+
+      <h2 className="mb4">Our top picks</h2>
+
+      <ItemRow
+        logo="logos/jet.png"
+        brandName="Jet"
+        pointRate="80 PER $1"
+        oldRate="40 PER $1"
+        button="Shop Now"
+        link="https://b.ewd.io/BFLastJet"
+      />
+
+      <ItemRow
+        logo="logos/barkbox.png"
+        brandName="BarkBox"
+        pointRate="40,000"
+        oldRate="20,000 "
+        button="Shop Now"
+        link="https://b.ewd.io/BFLastBarkBox"
+      />
+
+      <ItemRow
+        logo="logos/casper.png"
+        brandName="Casper"
+        pointRate="100 PER $1"
+        oldRate="75 PER $1"
+        button="Shop Now"
+        link="https://b.ewd.io/BFLastCasperUS"
+      />
+    </div>
+    <div className="divider" style={{ marginTop: 0 }} />
+
+    <h2 className="mb0">Boosted offers</h2>
+
+    <Image
+      href="https://b.ewd.io/BFLastChance2"
+      width="600"
+      src="black-friday-last-chance-no-desktop/image.png"
+      style={{ width: '100%', maxWidth: 600 }}
+    />
+
+    <div className="p2 mx-auto mb4" style={{ maxWidth: 300 }}>
+      <Button href="https://b.ewd.io/BFLastChance">Shop all</Button>
+    </div>
+
+    <Footer />
+  </Main>
+)
+
+export default BlackFridayEmail
