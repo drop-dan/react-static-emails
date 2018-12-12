818c1d535f7c1cb5e1bac95bde69d896a81ebe91
Krista Wymenga <krista@earnwithdrop.com>
2018-12-12 10:10:42 -0500

    Adds new email

---
+++ emails/12days/ca.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { Offer, DefaultTemplate, Button, Image } from '../../src/components'
+
+const CasperEmail = () => (
+  <DefaultTemplate
+    preheaderText="12 days. 12 special offers. Check back daily for limited time offers, on now thru December 22nd!"
+    hero="12days/12days-header.jpg"
+    title="This holiday season, give the gift of no re-gift."
+    body="12 days. 12 special offers. Check back daily for limited time offers, on now thru December 22nd!"
+    button={<Button>Shop All</Button>}
+  >
+      <Image
+    href="https://b.ewd.io/12GiftagramCA"
+    className="width-100"
+    width="600"
+    src="12days/1-giftagram.jpg"
+    />
+
+          <Image
+    href="https://b.ewd.io/12RocketmilesCA"
+    className="width-100"
+    width="600"
+    src="12days/2-rocketmiles.jpg"
+    /> 
+
+       <Image
+    href="https://b.ewd.io/12UnderArmourCA"
+    className="width-100"
+    width="600"
+    src="12days/3-underarmour.jpg"
+    />
+
+       <Image
+    href="https://b.ewd.io/12IndigoCA"
+    className="width-100"
+    width="600"
+    src="12days/4-indigo.jpg"
+    />
+
+       <Image
+    href="https://b.ewd.io/12LinksCA"
+    className="width-100"
+    width="600"
+    src="12days/5-linkslondon.jpg"
+    />
+
+       <Image
+    href="hhttps://b.ewd.io/12SephoraCA"
+    className="width-100"
+    width="600"
+    src="12days/6-sephora.jpg"
+    />
+
+           <Image
+    href="https://b.ewd.io/1223andme"
+    className="width-100"
+    width="600"
+    src="12days/7-23andme.jpg"
+    />
+
+           <Image
+    href="https://b.ewd.io/12GapCA"
+    className="width-100"
+    width="600"
+    src="12days/8-gap.jpg"
+    />
+
+           <Image
+    href="https://b.ewd.io/12HelloFreshCA"
+    className="width-100"
+    width="600"
+    src="12days/9-hellofresh.jpg"
+    />
+
+           <Image
+    href="https://b.ewd.io/12GrouponCA"
+    className="width-100"
+    width="600"
+    src="12days/10-groupon.jpg"
+    />
+
+               <Image
+    href="https://b.ewd.io/12KoboCA"
+    className="width-100"
+    width="600"
+    src="12days/11-koboca.jpg"
+    />
+
+               <Image
+    href="https://b.ewd.io/12KeurigCA"
+    className="width-100"
+    width="600"
+    src="12days/12-keurig.jpg"
+    />
+
+  </DefaultTemplate>
+)
+
+export default CasperEmail
